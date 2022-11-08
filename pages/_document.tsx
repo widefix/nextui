ed3e43a3eaecc15fa5bfc9e587de6a633d9c4225
Andrei Kaleshka <ka8725@gmail.com>
2022-11-08 18:55:39 +0100

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import { CssBaseline } from '@nextui-org/react'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import React from 'react'
+
+class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default AppDocument
